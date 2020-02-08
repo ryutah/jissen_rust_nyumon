c914d33fc046cdbd974db73105cee1690c7c7e6e
ryutah <ryuta0221@gmail.com>
2020-02-08 21:58:02 +0900

    feat(ch04): adds ch04 example 2

---
+++ ch04/ex04/examples/ch04_02_bool.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let b1 = true;
+    let b2 = !b1;
+
+    let n1 = 8;
+    let n2 = 12;
+    let b3 = n1 >= 10;
+    let b4 = n2 >= 10;
+    let b5 = b3 && b4;
+    let b6 = b3 || b4;
+
+    println!(
+        "b1: {}, b2: {} n1: {}, n2: {}, b3: {}, b4: {}, b5: {}, b6: {}",
+        b1, b2, n1, n2, b3, b4, b5, b6
+    );
+    assert_eq!(std::mem::size_of::<bool>(), 1)
+}
