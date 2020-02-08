f031006108ccc7977e6d0fe79c8fd498afdaeddd
ryutah <ryuta0221@gmail.com>
2020-02-08 21:08:40 +0900

    feat(ch03): create ch03 bionic sorter

---
+++ ch03/bitonic-sorter/src/utils.rs
@@ -0,0 +1,24 @@
+use super::SortOrder::{self, *};
+use rand::distributions::Standard;
+use rand::{Rng, SeedableRng};
+use rand_pcg::Pcg64Mcg;
+
+pub fn new_u32_vec(n: usize) -> Vec<u32> {
+    let mut rng = Pcg64Mcg::from_seed([0; 16]);
+    rng.sample_iter(&Standard).take(n).collect()
+}
+
+pub fn is_sorted_ascending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] <= pair[1])
+}
+
+pub fn is_sorted_descending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] >= pair[1])
+}
+
+pub fn is_sorted<T: Ord>(x: &[T], order: SortOrder) -> bool {
+    x.windows(2).all(|pair| match order {
+        Ascending => pair[0] <= pair[1],
+        Descending => pair[0] >= pair[1],
+    })
+}
