8e3dc1de7ed55d2b38017b23ea675ba8f34379e8
ryutah <ryuta0221@gmail.com>
2020-02-09 15:04:28 +0900

    feat(ch04): adds float example 6

---
+++ ch04/ex04/examples/ch04_06_float.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let f1 = 10.0; // f64
+    let f2 = 1_234.56f32; // f32
+    let f3 = 578.6E+77; // f64型(指数部指定)
+
+    println!("f1: {}", f1);
+    println!("f2: {}", f2);
+    println!("f3: {}", f3);
+}
